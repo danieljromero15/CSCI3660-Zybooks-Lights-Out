9a8af069719849011e59e4a4ffe0be174d2b9c21
Daniel Romero <danieljromero305@gmail.com>
2024-02-08 12:19:31 -0500

    Completed 3.4

--- app/src/main/java/com/zybooks/lightsout/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsout/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private GridLayout mLightGrid;
     private int mLightOnColor;
     private int mLightOffColor;
+    private final String GAME_STATE = "gameState";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +38,19 @@ public class MainActivity extends AppCompatActivity {
         mLightOffColor = ContextCompat.getColor(this, R.color.black);
 
         mGame = new LightsOutGame();
-        startGame();
+        if (savedInstanceState == null) {
+            startGame();
+        }else{
+            String gameState = savedInstanceState.getString(GAME_STATE);
+            mGame.setState(gameState);
+            setButtonColors();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString(GAME_STATE, mGame.getState());
     }
 
     private void startGame() {
